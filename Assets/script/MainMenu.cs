33c187cb0a833b23d73b7c897ce1cb67d0b8223f
TiaraOryza <85074230+TiaraOryza@users.noreply.github.com>
2022-06-16 11:25:18 +0700

    initial commit
    initial commit

---
+++ Assets/script/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void MulaiGame()
+    {
+        SceneManager.LoadScene("LevelEasy");
+    }
+
+    
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}

7458a4c68b26f09c8981287f4012a40a24dc98f3
TiaraOryza <85074230+TiaraOryza@users.noreply.github.com>
2022-06-23 10:18:38 +0700

    menambahi timer

--- Assets/TimerSetting.cs
+++ Assets/TimerSetting.cs
@@ -2,15 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour
+public class TimerSetting : MonoBehaviour
 {
-
-    private float hp;
-
-    public float hp
-    {
-        return hp;
-    }
     // Start is called before the first frame update
     void Start()
     {
